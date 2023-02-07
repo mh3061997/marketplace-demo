e820e3b475d472ca2e1f4df8bd7eb0946f9d9efa
mohamed.haitham <mohamed.haitham@systemsltd.com>
2023-02-07 13:26:00 +0200

    add models

--- src/main/java/com/example/marketplacedemo/models/Buyer.java
+++ src/main/java/com/example/marketplacedemo/models/Buyer.java
@@ -1,4 +1,17 @@
 package com.example.marketplacedemo.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
 public class Buyer {
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY )
+    private long id;
+    private String name;
+    @OneToMany
+    private List<Order> orders;
 }
--- src/main/java/com/example/marketplacedemo/models/Order.java
+++ src/main/java/com/example/marketplacedemo/models/Order.java
@@ -1,4 +1,16 @@
 package com.example.marketplacedemo.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Order {
+
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY )
+    private long id;
+    private double amount;
+    @ManyToOne
+    private Buyer buyer;
 }
