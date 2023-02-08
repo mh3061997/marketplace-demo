cea90e2308b158cc4fec749bfd44b98c590e3d00
mohamed.haitham <mohamed.haitham@systemsltd.com>
2023-02-08 14:50:25 +0200

    Add Authentication

---
+++ src/main/java/com/example/marketplacedemo/security/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.example.marketplacedemo.security.repository;
+
+import java.util.Optional;
+
+import com.example.marketplacedemo.security.models.ERole;
+import com.example.marketplacedemo.security.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
